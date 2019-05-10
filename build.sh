9c791f58fc5e07c3db17f2ff7bba9c18027dd31f
xonix <xonixx@gmail.com>
2019-05-10 15:25:13 +0300

    build.sh + work on CLI

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+GRAAL=~/soft/graalvm-ce-19.0.0
+UPX=~/soft/upx-3.95-amd64_linux
+
+mvn clean compile
+CP=$(mvn -q exec:exec -Dexec.executable=echo -Dexec.args="%classpath")
+
+cd target/classes
+
+echo
+echo Graal
+echo
+
+$GRAAL/bin/native-image -cp "$CP" com.cmlteam.serv.Serv
+ls -lh ./com.cmlteam.serv.serv
+
+echo
+echo Upx
+echo
+
+rm -f ./serv
+$UPX/upx ./com.cmlteam.serv.serv -oserv
+ls -lh ./serv
+
--- src/main/java/com/cmlteam/serv/Serv.java
+++ src/main/java/com/cmlteam/serv/Serv.java
@@ -3,6 +3,7 @@ package com.cmlteam.serv;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.apache.commons.cli.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,8 +14,38 @@ import java.nio.file.Path;
 
 public class Serv {
   public static void main(String[] args) throws Exception {
+
+    Options options = new Options();
+
+    Option input = new Option("i", "input", true, "input file path");
+    input.setRequired(true);
+    options.addOption(input);
+
+    Option output = new Option("o", "output", true, "output file");
+    output.setRequired(true);
+    options.addOption(output);
+
+    CommandLineParser parser = new DefaultParser();
+    HelpFormatter formatter = new HelpFormatter();
+    CommandLine cmd = null;
+
+    try {
+      cmd = parser.parse(options, args);
+    } catch (ParseException e) {
+      System.out.println(e.getMessage());
+      formatter.printHelp("utility-name", options);
+
+      System.exit(1);
+    }
+
+    String inputFilePath = cmd.getOptionValue("input");
+    String outputFilePath = cmd.getOptionValue("output");
+
+    System.out.println(inputFilePath);
+    System.out.println(outputFilePath);
+
     System.out.println("Starting");
-    System.exit(0);
+//    System.exit(0);
     HttpServer server = HttpServer.create(new InetSocketAddress(17777), 0);
     server.createContext("/dl", new MyHandler());
     server.setExecutor(null); // creates a default executor
