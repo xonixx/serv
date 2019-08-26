2208e985fbdf424762260e6e28db9a50417cbc7d
xonix <xonixx@gmail.com>
2019-08-26 22:04:40 +0300

    Add test suite #13

--- src/test/java/com/cmlteam/serv/ServTests.java
+++ src/test/java/com/cmlteam/serv/ServTests.java
@@ -1,11 +1,10 @@
 package com.cmlteam.serv;
 
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.URL;
 import java.nio.channels.Channels;
@@ -38,6 +37,42 @@ class ServTests {
     FileOutputStream fileOutputStream = new FileOutputStream(resultFile);
 
     fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+
+    assertEquals(inputFile.length(), resultFile.length(), "file size must be same");
+    assertThat(resultFile)
+        .describedAs("files should have same content")
+        .hasSameContentAs(inputFile);
+
+    serv.stop();
+  }
+
+  @Test
+  void testServeSingleFileCompressed(@TempDir Path tempDir) throws IOException {
+    Path file = tempDir.resolve("file.txt");
+
+    Files.write(file, "hello world 123".getBytes(StandardCharsets.UTF_8));
+
+    File inputFile = file.toFile();
+    Serv serv = new Serv(new String[] {inputFile.getAbsolutePath()});
+
+    InetSocketAddress address = serv.getAddress();
+
+    ReadableByteChannel readableByteChannel =
+        Channels.newChannel(
+            new URL("http://" + address.getHostName() + ":" + address.getPort() + "/dl?z")
+                .openStream());
+
+    File resultFileGz = tempDir.resolve("file_result.txt.gz").toFile();
+
+    FileOutputStream fileOutputStream = new FileOutputStream(resultFileGz);
+    fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+
+    GzipCompressorInputStream gzipCompressorInputStream =
+        new GzipCompressorInputStream(new BufferedInputStream(new FileInputStream(resultFileGz)));
+
+    File resultFile = tempDir.resolve("file_result.txt").toFile();
+
+    Files.copy(gzipCompressorInputStream, resultFile.toPath());
 
     assertEquals(inputFile.length(), resultFile.length(), "file size must be same");
     assertThat(resultFile)
