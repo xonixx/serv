eae6c517617be0c67e1ba766c87cb0b96985e6fc
xonix <xonixx@gmail.com>
2019-09-05 22:08:00 +0300

    Tests for Files Listing functionality #21

--- src/test/java/com/cmlteam/serv/ListingTests.java
+++ src/test/java/com/cmlteam/serv/ListingTests.java
@@ -1,6 +1,7 @@
 package com.cmlteam.serv;
 
 import com.cmlteam.util.Util;
+import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -115,38 +116,16 @@ class ListingTests {
   @Test
   void complexFileSetListingTest(@TempDir Path tempDir) throws IOException {
     // GIVEN
-    String folderName1 = "input_folder1";
-    Path inputFolder1 = createTestFolder(tempDir, folderName1);
-
-    String fname1 = "file1.txt";
-    String fname2 = "file2.txt";
-    String fname3 = "file3";
-
-    Path file1 = createTestFile(inputFolder1, fname1, "hello world 123");
-    Path file2 = createTestFile(inputFolder1, fname2, "");
-    Path file3 =
-        createTestFile(inputFolder1, fname3, "123\n456\n789000000000000000000000000000000000");
-
-    String folderName2 = "input_folder2";
-    Path inputFolder2 = createTestFolder(tempDir, folderName2);
-
-    String fname4 = "file4.txt";
-    String fname5 = "file5.csv";
-
-    Path file4 = createTestFile(inputFolder2, fname4, "hello world 123");
-    Path file5 = createTestFile(inputFolder2, fname5, "");
-
-    String fname7 = "FiLe777";
-    Path file7 = createTestFile(tempDir, fname7, "");
+    GivenForComplexFileSet given01 = GivenForComplexFileSet.prepare(tempDir);
 
     serv =
         new Serv(
             new String[] {
               "-p",
               testPort,
-              inputFolder1.toFile().getAbsolutePath(),
-              inputFolder2.toFile().getAbsolutePath(),
-              file7.toFile().getAbsolutePath()
+              given01.inputFolder1.toFile().getAbsolutePath(),
+              given01.inputFolder2.toFile().getAbsolutePath(),
+              given01.file7.toFile().getAbsolutePath()
             });
 
     InetSocketAddress address = serv.getAddress();
@@ -165,12 +144,132 @@ class ListingTests {
 
     assertEquals("Index of /", document.select("h1").first().text());
     assertEquals(3, trElements.size());
-    assertEquals(folderName1, trElements.get(0).select("td").first().text());
-    assertEquals(folderName2, trElements.get(1).select("td").first().text());
-    assertEquals(fname7, trElements.get(2).select("td").first().text());
+    assertEquals(given01.folderName1, trElements.get(0).select("td").first().text());
+    assertEquals(given01.folderName2, trElements.get(1).select("td").first().text());
+    assertEquals(given01.fname7, trElements.get(2).select("td").first().text());
     assertEquals("", trElements.get(0).select("td").get(1).text());
     assertEquals("", trElements.get(1).select("td").get(1).text());
     assertEquals(
-        Util.renderFileSize(file7.toFile().length()), trElements.get(2).select("td").get(1).text());
+        Util.renderFileSize(given01.file7.toFile().length()),
+        trElements.get(2).select("td").get(1).text());
+  }
+
+  @Test
+  void complexFileSetListingNavigationTest(@TempDir Path tempDir) throws IOException {
+    // GIVEN
+    GivenForComplexFileSet given = GivenForComplexFileSet.prepare(tempDir);
+
+    serv =
+        new Serv(
+            new String[] {
+              "-p",
+              testPort,
+              given.inputFolder1.toFile().getAbsolutePath(),
+              given.inputFolder2.toFile().getAbsolutePath(),
+              given.file7.toFile().getAbsolutePath()
+            });
+
+    InetSocketAddress address = serv.getAddress();
+
+    String listingUrl = "http://" + address.getHostName() + ":" + address.getPort() + "/listing";
+
+    //    System.out.println(getUrlToString(listingUrl));
+
+    Document documentRoot = Jsoup.connect(listingUrl).get();
+
+    String nextPage =
+        documentRoot
+            .select("table tbody tr")
+            .get(1)
+            .select("td")
+            .first()
+            .select("a")
+            .first()
+            .attr("href");
+    String listingUrlNext = "http://" + address.getHostName() + ":" + address.getPort() + nextPage;
+
+    // WHEN
+    // emulate click on a folder 'input_folder2' link
+    Document document = Jsoup.connect(listingUrlNext).get();
+
+    // THEN
+    System.out.println(document);
+
+    Elements trElements = document.select("table tbody tr");
+
+    assertEquals("Index of /" + given.folderName2 + "/", document.select("h1").first().text());
+    assertEquals(2, trElements.size());
+    assertEquals(given.fname4, trElements.get(0).select("td").first().text());
+    assertEquals(given.fname5, trElements.get(1).select("td").first().text());
+    assertEquals(
+        Util.renderFileSize(given.file4.toFile().length()),
+        trElements.get(0).select("td").get(1).text());
+    assertEquals(
+        Util.renderFileSize(given.file5.toFile().length()),
+        trElements.get(1).select("td").get(1).text());
+  }
+
+  @RequiredArgsConstructor
+  private static class GivenForComplexFileSet {
+    private final String folderName1;
+    private final Path inputFolder1;
+    private final String fname1;
+    private final String fname2;
+    private final String fname3;
+    private final Path file1;
+    private final Path file2;
+    private final Path file3;
+    private final String folderName2;
+    private final Path inputFolder2;
+    private final String fname4;
+    private final String fname5;
+    private final Path file4;
+    private final Path file5;
+    private final String fname7;
+    private final Path file7;
+
+    private static GivenForComplexFileSet prepare(Path tempDir) throws IOException {
+      String folderName1 = "input_folder1";
+      Path inputFolder1 = createTestFolder(tempDir, folderName1);
+
+      String fname1 = "file1.txt";
+      String fname2 = "file2.txt";
+      String fname3 = "file3";
+
+      Path file1 = createTestFile(inputFolder1, fname1, "hello world 123");
+      Path file2 = createTestFile(inputFolder1, fname2, "");
+      Path file3 =
+          createTestFile(inputFolder1, fname3, "123\n456\n789000000000000000000000000000000000");
+
+      String folderName2 = "input_folder2";
+      Path inputFolder2 = createTestFolder(tempDir, folderName2);
+
+      String fname4 = "file4.txt";
+      String fname5 = "file5.csv";
+
+      Path file4 = createTestFile(inputFolder2, fname4, "hello world 123");
+      Path file5 = createTestFile(inputFolder2, fname5, "");
+
+      String fname7 = "FiLe777";
+      Path file7 = createTestFile(tempDir, fname7, "");
+
+      return new GivenForComplexFileSet(
+          folderName1,
+          inputFolder1,
+          fname1,
+          fname2,
+          fname3,
+          file1,
+          file2,
+          file3,
+          folderName2,
+          inputFolder2,
+          fname4,
+          fname5,
+          file4,
+          file5,
+          fname7,
+          file7);
+    }
   }
 }
